48e99852af3fcb70124af7fa77686fb5a0aa7e67
PHUC3652 <phuc3652@gmail.com>
2024-10-06 11:03:35 +0700

    Revert "PHUCHUYNH2"
    This reverts commit f370b56ac008fed694763dcba5490f557d8216f8.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> huynh phuc123
+          Edit <code>src/App.js</code> huynh phuc
         </p>
         <a
           className="App-link"
