76a3b7c4a2826fcc99fd2aad837063bf68a96f30
PHUC3652 <phuc3652@gmail.com>
2024-10-06 10:14:25 +0700

    HUYNH

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> commit 123456789123123
+          Edit <code>src/App.js</code> commit 123456789123123123123
         </p>
         <a
           className="App-link"
